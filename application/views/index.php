7ae7a3632e415e7e32748999d52d324181bbf1cd
Muhammad Nur Hidayat <seebeb@gmail.com>
2011-12-05 02:02:50 +0700

    inisialisasi config,autoload,file template boilerplate

---
+++ application/views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo (isset($title)) ? $title . ' - ' : ''; ?>Monev</title>
+
+        <!-- HTML5, for IE6-8 support of HTML elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <base href="<?php echo base_url(); ?>"></base>
+        <!-- styles load below -->
+        
+    </head>
+    <body>
+		<!-- template load below -->
+	
+		<!-- javascript load below -->
+	</body>
+</html>
