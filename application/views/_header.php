0564dd68c977c4fbd7b35be677631dbd9f3b5cbc
Heilbert Mapaly <hb1xrs@gmail.com>
2011-12-28 17:25:26 +0700

    loginform

--- application/views/_header.php
+++ application/views/_header.php
@@ -1,5 +1,7 @@
 		<img src="<?php echo ASSETS_DIR_IMG.'title.png'?>" id="logo"/>
 		<div id="user">Widy Agung Priasmoro</div>
 		<div id="position">SATKER</div>
-		<a href="#" class="custom floatright">Logout</a>
+		<!--<a href="#" class="custom floatright"> </a>-->
+		<?php echo anchor('user/logout','Logout');?>
+		
 		
