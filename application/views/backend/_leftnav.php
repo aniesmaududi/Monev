905b8ec790fcd74c693a430c294196e5f0c3183d
Radita Liem <raymerta@gmail.com>
2012-01-17 14:09:40 +0700

    penambahan pada navigasi

--- application/views/backend/_leftnav.php
+++ application/views/backend/_leftnav.php
@@ -1,11 +1,11 @@
 		<div id="content-left">
-			<a class="navigation-top" href="<?php echo site_url('backend')?>">Dashboard</a>
-			<a class="navigation-title" href="<?php echo site_url('backend/user')?>" style="border-bottom:none;">Manajemen Administrator</a>
-			<a class="navigation-title" href="<?php echo site_url('backend/access_management')?>" style="border-bottom:none;">Manajemen Akses</a>
-			<a class="navigation-title" href="<?php echo site_url('backend/sms/outbox')?>" style="border-bottom:none" id="active-main">Broadcast SMS</a>
-			<a class="navigation-title" href="<?php echo site_url('backend/ref')?>" style="border-bottom:none;">Pemeliharaan Referensi</a>
-			<a class="navigation-title" href="<?php echo site_url('backend/audit')?>">Audit Trail</a>
-			<a class="navigation-bottom" href="<?php echo site_url('backend/queue')?>">Antrian</a>			
+			<a class="navigation-top" href="<?php echo site_url('backend')?>" <?php if($this->uri->segment(2) == ''):echo 'id="active-main"';endif;?>>Dashboard</a>
+			<a class="navigation-title" href="<?php echo site_url('backend/user')?>" style="border-bottom:none;" <?php if($this->uri->segment(2) == 'user'):echo 'id="active-main"';endif;?>>Manajemen Administrator</a>
+			<a class="navigation-title" href="<?php echo site_url('backend/access_management')?>" style="border-bottom:none;" <?php if($this->uri->segment(2) == 'access_management'):echo 'id="active-main"';endif;?>>Manajemen Akses</a>
+			<a class="navigation-title" href="<?php echo site_url('backend/sms/outbox')?>" style="border-bottom:none" <?php if($this->uri->segment(2) == 'sms'):echo 'id="active-main"';endif;?>>Broadcast SMS</a>
+			<a class="navigation-title" href="<?php echo site_url('backend/ref')?>" style="border-bottom:none;" <?php if($this->uri->segment(2) == 'ref'):echo 'id="active-main"';endif;?>>Pemeliharaan Referensi</a>
+			<a class="navigation-title" href="<?php echo site_url('backend/audit')?>" <?php if($this->uri->segment(2) == 'audit'):echo 'id="active-main"';endif;?>>Audit Trail</a>
+			<a class="navigation-bottom" href="<?php echo site_url('backend/queue')?>" <?php if($this->uri->segment(2) == 'queue'):echo 'id="active-main"';endif;?>>Antrian</a>			
 
 		</div><!-- end of content-left -->
 		
