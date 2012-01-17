e0721e5c6cd673c61578b2323f1fa810257c3d4d
Radita Liem <raymerta@gmail.com>
2012-01-17 08:44:18 +0700

    perbaikan dashboard kementrian

--- application/views/_header.php
+++ application/views/_header.php
@@ -1,8 +1,8 @@
 		<script type="text/javascript" src="<?php echo ASSETS_DIR_JS;?>fusioncharts.js"></script>
         <img src="<?php echo ASSETS_DIR_IMG.'title.png'?>" id="logo"/>
         <a href="user/logout" class="custom floatright" id="logout">Logout</a>
-		<div id="position"><?php echo $this->session->userdata('jabatan');?></div>
-		<div id="user"><?php echo $this->session->userdata('nama');?></div>
+		<div id="position"><?php echo strtoupper($this->session->userdata('jabatan'));?></div>
+		<div id="user"><?php echo strtoupper($this->session->userdata('nama'));?></div>
 		
 		
 		
