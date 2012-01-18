52258bab301f22b71242e0dd70a790871d7f9de5
Heilbert Mapaly <hb1xrs@gmail.com>
2012-01-18 23:41:53 +0700

    index dan controler

--- application/views/backend/queue/index.php
+++ application/views/backend/queue/index.php
@@ -10,11 +10,17 @@ font-size:10px;-moz-border-radius: 6px;-webkit-border-radius: 6px;
 
 	<h1><?php echo $title;?></h1>
 	<div id="search-box" style="min-height:400px;">
-
+				<span class="custom-button-span"></span>
 	<?php
     if ($this->session->flashdata('message')):
         echo flash_message($this->session->flashdata('message_type'));
     endif;
+	echo anchor('backend/queue/index','Sedang di Proses','class=custom');
+	echo anchor('backend/queue/done','Sudah di Proses','class=custom');
+	echo anchor('backend/queue/success','Berhasil Proses','class=custom');
+	echo anchor('backend/queue/fail','Gagal Proses','class=custom');
+	if($rows->num_rows()>0)
+	{
 	?>
 	<table id="table1" class="backend-table">
 
@@ -35,6 +41,8 @@ font-size:10px;-moz-border-radius: 6px;-webkit-border-radius: 6px;
 				<td><?php echo $value->kdsatker.' -- '.$value->nmsatker ?></td>
 				<td><?php if($value->is_done == 1){ echo "sukses"; } else { echo "belum diproses"; } ?></td>
 				<?php $nama=$value->kddept.' -- '.$value->nmdept.' -- '.$value->kdunit.' -- '.$value->nmunit.' -- '.$value->kdsatker.' -- '.$value->nmsatker;?>
+				<!--<td><?php if($value->is_done != 1){ ?><input type="hidden" value="<?php echo $nama ?>" name="nama"><input class="proses" type="button" value="Proses" onclick="OnClickButton(<? echo $value->id; ?>)"><?php } else { echo "-";}?></td>-->
+				<!--<td><?php// if($value->is_done != 1){ echo anchor('/backend/queue/proses/' . $value->id, 'Proses','class=.flash'); } else { echo "-";}?></td>-->
 				<?php if($temp==0)
 				$temp=$value->id; ?>
 
@@ -42,11 +50,11 @@ font-size:10px;-moz-border-radius: 6px;-webkit-border-radius: 6px;
 			<?php endforeach;?>
 		</tbody>
 	</table>
-
+	<?php }else{ echo '<br><br>No Data';}?>
 </div>
 
 <div id="nav-box">
-	<div class="clearfix"></div>
+<div class="clearfix"></div>
 </div>
 
 
@@ -70,5 +78,5 @@ function setupRefresh(){
 function refreshPage(){
 	reloadpage();
 	window.location="backend/queue/proses/" + <?php echo $temp?>;
-} 
+}
 </script>
